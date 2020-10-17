8d047200c238531d0228b4bda493e8cb45d72a45
Daniel Schroeder <daniel.schroederHH@gmail.com>
2020-10-17 14:50:40 +0200

    Add enums Action and Result to demonstrate results among player actions

---
+++ src/main/kotlin/Action.kt
@@ -0,0 +1,28 @@
+enum class Action {
+    ROCK {
+        override fun fights(other: Action) =
+            when (other) {
+                ROCK -> Result.DRAW
+                PAPER -> Result.LOSS
+                SCISSOR -> Result.WIN
+            }
+    },
+    PAPER {
+        override fun fights(other: Action) =
+            when (other) {
+                ROCK -> Result.WIN
+                PAPER -> Result.DRAW
+                SCISSOR -> Result.LOSS
+            }
+    },
+    SCISSOR {
+        override fun fights(other: Action) =
+            when (other) {
+                ROCK -> Result.LOSS
+                PAPER -> Result.WIN
+                SCISSOR -> Result.DRAW
+            }
+    };
+
+    abstract fun fights(other: Action): Result
+}
