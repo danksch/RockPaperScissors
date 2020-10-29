4203f0a885adc829217880274fd487a2681acaa8
Daniel Schroeder <daniel.schroederHH@gmail.com>
2020-10-29 18:53:59 +0100

    Add Round test which tests printing of result

--- src/test/kotlin/RoundTest.kt
+++ src/test/kotlin/RoundTest.kt
@@ -6,6 +6,8 @@ import io.mockk.every
 import io.mockk.spyk
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 
 class RoundTest {
     @Test
@@ -25,5 +27,26 @@ class RoundTest {
         val round = Round(playerOne, playerTwo)
         round.start()
         assertEquals(round.resultMessage, "Round ended in a draw!")
+    }
+
+    @Test
+    fun whenRoundEnds_withPlayerTwoWinning_resultMessageIsBuiltCorrectly() {
+        val playerOne = spyk<RegularPlayer>()
+        every { playerOne.act() } returns Action.SCISSORS
+        val playerTwo = RockPlayer()
+        val round = Round(playerOne, playerTwo)
+        round.start()
+        assertEquals(round.resultMessage, "Rock Player wins!")
+    }
+
+    @Test
+    fun whenRoundEnds_andPrintRoundResultIsTrue_resultMessageIsPrinted() {
+        val playerOne = RockPlayer()
+        val playerTwo = RockPlayer()
+        val round = Round(playerOne, playerTwo, true)
+        val stream = ByteArrayOutputStream()
+        System.setOut(PrintStream(stream))
+        round.start()
+        assertEquals(stream.toString().trim(), "ROCK vs. ROCK: Round ended in a draw!")
     }
 }
